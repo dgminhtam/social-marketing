d667fa0bc26ba82804ac1a6758c6f3aa93019ea7
Minh Tam <35762319+dgminhtam@users.noreply.github.com>
2024-12-30 22:20:00 +0700

    update

---
+++ src/main/java/com/social/marketing/order/controller/ClientOrderController.java
@@ -0,0 +1,42 @@
+package com.social.marketing.order.controller;
+
+import com.social.marketing.order.entity.Order;
+import com.social.marketing.order.model.request.PlaceOrderRequest;
+import com.social.marketing.order.model.response.OrderDetailResponse;
+import com.social.marketing.order.model.response.OrderResponse;
+import com.social.marketing.order.model.response.PaymentResponse;
+import com.social.marketing.order.service.OrderService;
+import com.social.marketing.search.anotation.Search;
+import jakarta.annotation.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/client/orders")
+public class ClientOrderController {
+
+    @Resource
+    private OrderService orderService;
+
+    @PostMapping
+    public OrderResponse placeOrder(@RequestBody PlaceOrderRequest request) {
+        return orderService.placeOrder(request);
+    }
+
+    @PostMapping("/{code}/request-payment")
+    public PaymentResponse requestPayment(@PathVariable String code) {
+        return orderService.requestPayment(code);
+    }
+
+    @GetMapping("/{code}")
+    public OrderDetailResponse getOrderDetail(@PathVariable String code) {
+        return orderService.getOrderDetail(code);
+    }
+
+    @GetMapping("/by-email/{email}")
+    public Page<OrderResponse> getOrders(@PathVariable String email, @Search Specification<Order> specification, Pageable pageable) {
+        return orderService.getOrdersByEmail(email, specification, pageable);
+    }
+}
--- src/main/java/com/social/marketing/order/controller/OrderController.java
+++ src/main/java/com/social/marketing/order/controller/OrderController.java
@@ -1,17 +1,16 @@
 package com.social.marketing.order.controller;
 
 import com.social.marketing.order.entity.Order;
-import com.social.marketing.order.model.request.PlaceOrderRequest;
-import com.social.marketing.order.model.response.OrderDetailResponse;
 import com.social.marketing.order.model.response.OrderResponse;
-import com.social.marketing.order.model.response.PaymentResponse;
 import com.social.marketing.order.service.OrderService;
 import com.social.marketing.search.anotation.Search;
 import jakarta.annotation.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
@@ -19,26 +18,6 @@ public class OrderController {
 
     @Resource
     private OrderService orderService;
-
-    @PostMapping
-    public OrderResponse placeOrder(@RequestBody PlaceOrderRequest request) {
-        return orderService.placeOrder(request);
-    }
-
-    @PostMapping("/{code}/request-payment")
-    public PaymentResponse requestPayment(@PathVariable String code) {
-        return orderService.requestPayment(code);
-    }
-
-    @GetMapping("/{code}")
-    public OrderDetailResponse getOrderDetail(@PathVariable String code) {
-        return orderService.getOrderDetail(code);
-    }
-
-    @GetMapping("/by-email/{email}")
-    public Page<OrderResponse> getOrders(@PathVariable String email, @Search Specification<Order> specification, Pageable pageable) {
-        return orderService.getOrdersByEmail(email, specification, pageable);
-    }
 
     @GetMapping
     public Page<OrderResponse> getOrders(@Search Specification<Order> specification, Pageable pageable) {
